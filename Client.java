0c146c2e943ae7a42f62e4aaae90af67df14d4f9
kgnah <arnoldserus@gmail.com>
2026-06-25 10:10:13 +0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,8 +2,6 @@ public class Client {
 
     public static void main(String[] args) {
 
-        Board board = new Board();
-
         Player human = new HumanPlayer(1);
 
         Player computer = new RemoteComputerPlayer(2, "localhost", 5000);
