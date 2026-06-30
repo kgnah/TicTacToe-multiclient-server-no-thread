0d13ea00bcb056e283e350ca84caaa6e7bad0bdd
kgnah <arnoldserus@gmail.com>
2026-06-30 11:06:21 +0700

    Update RemoteComputerPlayer.java

--- RemoteComputerPlayer.java
+++ RemoteComputerPlayer.java
@@ -17,22 +17,28 @@ public class RemoteComputerPlayer extends Player {
         try {
             Socket socket = new Socket(host, port);
 
+            // receiving and sending data to server
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 
+            // encode board and send it to server
             output.println("BOARD:" + BoardProtocol.encode(board));
 
+            // server response
             String response = input.readLine();
 
+            // check if server response is valid
             if (response != null && response.startsWith("MOVE:")) {
                 int move = Integer.parseInt(response.substring(5).trim());
 
+                // update local board with computer mark for valid move
                 if (move != -1) {
                     board.setCell(move, mark);
                 }
             }
 
+            // Close connection after one request-response
             socket.close();
 
         } catch (IOException e) {
